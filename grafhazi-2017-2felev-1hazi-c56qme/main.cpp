b72a760fc5bc4702224f9469ad1d3bf4d4b9a54a
balazs <magyarb94@gmail.com>
2017-03-22 02:27:00 +0100

    started angle indicator triangle

--- grafhazi-2017-2felev-1hazi-c56qme/main.cpp
+++ grafhazi-2017-2felev-1hazi-c56qme/main.cpp
@@ -328,10 +328,13 @@ public:
 };
 
 
+
 class Triangle {
 	unsigned int vao;	// vertex array object id
+	unsigned int vbo[2]; //buffer
 	float sx, sy;		// scaling
 	float wTx, wTy;		// translation
+	std::vector<float> coords;
 public:
 	Triangle() {
 		Animate(0);
@@ -341,17 +344,11 @@ public:
 		glGenVertexArrays(1, &vao);	// create 1 vertex array object
 		glBindVertexArray(vao);		// make it active
 
-		unsigned int vbo[2];		// vertex buffer objects
+				// vertex buffer objects
 		glGenBuffers(2, &vbo[0]);	// Generate 2 vertex buffer objects
 
 									// vertex coordinates: vbo[0] -> Attrib Array 0 -> vertexPosition of the vertex shader
-		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]); // make it active, it is an array
-		static float vertexCoords[] = { -8, -8, -6, 10, 8, -2 };	// vertex data on the CPU
-		glBufferData(GL_ARRAY_BUFFER,      // copy to the GPU
-			sizeof(vertexCoords),  // number of the vbo in bytes
-			vertexCoords,		   // address of the data array on the CPU
-			GL_STATIC_DRAW);	   // copy to that part of the memory which is not modified 
-								   // Map Attribute Array 0 to the current bound vertex buffer (vbo[0])
+		
 		glEnableVertexAttribArray(0);
 		// Data organization of Attribute Array 0 
 		glVertexAttribPointer(0,			// Attribute Array 0
@@ -359,15 +356,35 @@ public:
 			GL_FALSE,		// not in fixed point format, do not normalized
 			0, NULL);     // stride and offset: it is tightly packed
 
-						  // vertex colors: vbo[1] -> Attrib Array 1 -> vertexColor of the vertex shader
-		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]); // make it active, it is an array
-		static float vertexColors[] = { 1, 0, 0, 0, 1, 0, 0, 0, 1 };	// vertex data on the CPU
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexColors), vertexColors, GL_STATIC_DRAW);	// copy to the GPU
+						  // vertex colors: vbo[1] -> Attrib Array 1 -> vertexColor of the vertex shader	
 
 																							// Map Attribute Array 1 to the current bound vertex buffer (vbo[1])
 		glEnableVertexAttribArray(1);  // Vertex position
 									   // Data organization of Attribute Array 1
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL); // Attribute Array 1, components/attribute, component type, normalize?, tightly packed
+	}
+
+	void FeedAngle(float angle) //meredekség
+	{
+		//beírja a float-vectorba a megfelelo összegeket
+	}
+
+	void RefreshAngle() {
+
+		//coords alapján kirajzolni a háromszöget
+
+		/*
+
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]); // make it active, it is an array
+
+		glBufferData(GL_ARRAY_BUFFER,      // copy to the GPU
+			sizeof(vertexCoords),  // number of the vbo in bytes
+			vertexCoords,		   // address of the data array on the CPU
+			GL_STATIC_DRAW);	   // copy to that part of the memory which is not modified 
+								   // Map Attribute Array 0 to the current bound vertex buffer (vbo[0])
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]); // make it active, it is an array
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexColors), vertexColors, GL_STATIC_DRAW);	// copy to the GPU*/
 	}
 
 	void Animate(float t) {
@@ -391,7 +408,7 @@ public:
 		else printf("uniform MVP cannot be set\n");
 
 		glBindVertexArray(vao);	// make the vao and its vbos active playing the role of the data source
-		glDrawArrays(GL_TRIANGLES, 0, 3);	// draw a single triangle with vertices defined in vao
+		glDrawArrays(GL_TRIANGLES, 0, coords.size()/2);	// draw a single triangle with vertices defined in vao
 	}
 };
 
@@ -846,8 +863,8 @@ public:
 	}
 
 	void Animate(float t) { //az eltelt ido a space lenyomasa ota
-		sx = 1; // *sinf(t);
-		sy = 1; // *cosf(t);
+		sx = 1;
+		sy = 1;
 		Vector loc;
 		if (spacepressedtime != 0) {
 			//ha vége a pályának, marad ott
@@ -863,13 +880,13 @@ public:
 					loc = lagrange.r(state);
 				}
 			}
-			wTx = loc.x;// 4 * cosf(t / 2);
-			wTy = loc.y;// 4 * sinf(t / 2);
+			wTx = loc.x;
+			wTy = loc.y;
 		}
 		else
 		{
-			wTx = 0;// 4 * cosf(t / 2);
-			wTy = 0;// 4 * sinf(t / 2);
+			wTx = 0;
+			wTy = 0;
 		}
 	}
 
